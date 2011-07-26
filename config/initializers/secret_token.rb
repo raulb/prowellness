1de7323900793f0351050ff075b942d05eb031ca
Fernando Blat <ferblape@gmail.com>
2011-07-25 22:23:31 -0400

    Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prowellness::Application.config.secret_token = 'c1f672d065fab46b0d94283c1333717a7c6bb0219602c87c0fe075be0863db9e90057a2506b4d6ded8f3d90e4d47cd55d93fa550d4fdd7fa483855a47cbece67'
