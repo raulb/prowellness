a574975c948470db62a5a6044977268635aff762
Raúl Barroso <ra.barroso@gmail.com>
2011-08-08 18:45:27 +0200

    Changes for readme file

---
+++ README.md
@@ -0,0 +1,40 @@
+Prowelleness
+============
+
+Steps to configure the project correctly
+
+Instalation
+-----------
+
+The main folder contains the `.rvmrc` file. This set a gemset with Ruby 1.9.2-p290 version.
+
+1. `rvm install ruby-1.9.2-p290`
+2. `gem install bundler`
+
+This gem require a RubyGems version >= 1.3.6. If there isn't:
+
+`gem update --system`
+
+3. `rvm use ruby-1.9.2-p290@prowellness`
+4. `bundle`
+
+If there is an error with linecache19 or ruby-debug-base19 gems, do:
+
+`gem install linecache19 --backtrace` or `gem install ruby-debug-base19` (if it's case)
+
+And `bundle` again to following
+
+* NOTA: There are dependencies with postgres (gem pg).
+* If not exists. You have to install it. For example, with brew: `brew install postgres` and then `env ARCHFLAGS="-arch x86_64" gem install pg`
+
+### Database
+
+PostgreSQL, in development, testing and production.
+
+`rake db:create`
+`rake db:migrate`
+`rake db:seed`
+
+### Testing
+
+`RAILS_ENV=test rake db:drop db:create db:migrate spec:models spec:acceptance`
---
+++ lib/github/markups.rb
@@ -0,0 +1,38 @@
+markup(:markdown, /md|mkdn?|mdown|markdown/) do |content|
+  Markdown.new(content).to_html
+end
+
+markup(:redcloth, /textile/) do |content|
+  RedCloth.new(content).to_html
+end
+
+markup('github/markup/rdoc', /rdoc/) do |content|
+  GitHub::Markup::RDoc.new(content).to_html
+end
+
+markup('org-ruby', /org/) do |content|
+  Orgmode::Parser.new(content).to_html
+end
+
+markup(:creole, /creole/) do |content|
+  Creole.creolize(content)
+end
+
+markup(:wikicloth, /mediawiki|wiki/) do |content|
+  WikiCloth::WikiCloth.new(:data => content).to_html(:noedit => true)
+end
+
+command(:rest2html, /re?st(\.txt)?/)
+
+command('asciidoc -s --backend=xhtml11 -o - -', /asciidoc/)
+
+# pod2html is nice enough to generate a full-on HTML document for us,
+# so we return the favor by ripping out the good parts.
+#
+# Any block passed to `command` will be handed the command's STDOUT for
+# post processing.
+command("/usr/bin/env perl -MPod::Simple::HTML -e Pod::Simple::HTML::go", /pod/) do |rendered|
+  if rendered =~ /<!-- start doc -->\s*(.+)\s*<!-- end doc -->/mi
+    $1
+  end
+end
