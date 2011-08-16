80385e42f3e1e035664ebd9a7de1967b5da8f13d
Fernando Blat <ferblape@gmail.com>
2011-08-15 21:40:46 -0400

    20 posts in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+require File.expand_path("../../spec/support/posts_factory", __FILE__)
+require File.expand_path("../../spec/support/string_extension", __FILE__)
+
+include Delorean
+include Prowellness::Factories
+
+# Setup a new user
+
 admin = User.new
 admin.email = 'admin@example.com'
 admin.password = 'admin'
@@ -5,4 +13,12 @@ admin.password_confirmation = 'admin'
 admin.name_and_surname = 'Admin'
 admin.save
 admin.set_as_admin!
+admin.reload
 
+# Setup 20 blog posts in different days
+
+1.upto(20) do |i|
+  time_travel_to "#{21 - i} days ago"
+  post = create_post :title => "Ejercicios para programadores ##{i}", :categories => "blog", :user => admin
+  back_to_the_present
+end
