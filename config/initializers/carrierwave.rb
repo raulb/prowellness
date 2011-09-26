6529ef627f25372fba410c167c87b371837d1b35
Fernando Blat <ferblape@gmail.com>
2011-09-26 19:52:36 -0300

    link changed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,15 @@
+module CarrierWave
+  module MiniMagick
+    def quality(percentage)
+      manipulate! do |img|
+        img.quality(percentage)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+end
+
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
