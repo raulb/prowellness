453d7db4dd5ded057a611e17d0598a42d8e08e18
Raúl Barroso <ra.barroso@gmail.com>
2011-10-01 20:00:46 +0200

    rendering html in blog post -it should be modified

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -1,2 +1,16 @@
 //= require jquery
 //= require jquery_ujs
+
+jQuery(document).ready(function() {
+  
+  // At the beginning, we need to paste the original content to its editable elem
+  $('#edit_body').html($('#post_body').html());
+  
+  $('input[type="submit"]').click(function(event) {
+    console.log('body ', $('#edit_body').html());
+    console.log('body sin html ', $('#edit_body'));
+    
+    
+    $('#post_body').html($('#edit_body').html());
+  });
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery_ui
 //= require_directory .
 
 jQuery(document).ready(function($) {
