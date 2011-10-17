dbe69966c4f7fab3182df62104ab5ec5a5eafc82
Raúl Barroso <ra.barroso@gmail.com>
2011-10-17 19:44:27 +0200

    adding about content

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,7 +5,7 @@ class SiteController < ApplicationController
   def home
     @main_posts = Post.get_last_5_articles
     @posts = Post.filter_by_category('articulos').order_by_publish_date.
-                  limit(4).where("id not in (#{@main_posts.map(&:id).join(",")})")
+                  limit(0).where("id not in (#{@main_posts.map(&:id).join(",")})")
     @last_post = Post.last_blog_posts(1).first
   end
 
