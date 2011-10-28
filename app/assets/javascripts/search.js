5b87af4e3b0f5c3c3b1b8135a05cf4250767485d
Raúl Barroso <ra.barroso@gmail.com>
2011-10-29 01:50:54 +0200

    Search and other things added and improved

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function($) {
+  $('.filters').hide();
+  
+  $('.toggle_filters').click(function(e){
+    $('.filters').toggle();
+    e.stopPropagation(); e.preventDefault();
+  });
+
+  var page = 1,
+      loading = false;
+
+  function nearBottomOfPage() {
+    return $(window).scrollTop() > $('.post:last').position().top - 300;
+  }
+
+  $(window).scroll(function(){
+    if (loading) {
+      return;
+    }
+
+    if(nearBottomOfPage()) {
+      loading=true;
+      page++;
+      $.ajax({
+        url: $('a[rel=pagination]').attr('href') + '&page=' + page,
+        type: 'get',
+        dataType: 'script',
+        success: function() {
+          $(window).sausage('draw');
+          loading=false;
+        }
+      });
+    }
+  });
+
+  $(window).sausage();
+});
