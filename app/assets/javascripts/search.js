d0e44264a44e5848a3fd667a7117aeff5803f942
Raúl Barroso <ra.barroso@gmail.com>
2011-10-30 19:38:45 +0100

    Search full customized

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,7 +2,11 @@ jQuery(document).ready(function($) {
   $('.filters').hide();
   
   $('.toggle_filters').click(function(e){
-    $('.filters').toggle();
+    
+    $('.filters').slideToggle('fast');
+
+    // TODO: Should I clean filters when it's hidden?
+    
     e.stopPropagation(); e.preventDefault();
   });
 
@@ -10,7 +14,8 @@ jQuery(document).ready(function($) {
       loading = false;
 
   function nearBottomOfPage() {
-    return $(window).scrollTop() > $('.post:last').position().top - 300;
+    if ($('.post').length > 0) return $(window).scrollTop() > $('.post:last').position().top - 300;
+    return false;
   }
 
   $(window).scroll(function(){
@@ -34,4 +39,36 @@ jQuery(document).ready(function($) {
   });
 
   $(window).sausage();
+  
+  // Datepicker for search dates
+  var dates = $( "#date_from, #date_to" ).datepicker({
+    changeMonth: true,
+    numberOfMonths: 1,
+    onSelect: function( selectedDate ) {
+      var option = this.id == "date_from" ? "minDate" : "maxDate",
+      instance = $( this ).data( "datepicker" ),
+      date = $.datepicker.parseDate(
+        instance.settings.dateFormat ||
+        $.datepicker._defaults.dateFormat,
+        selectedDate, instance.settings );
+        dates.not( this ).datepicker( "option", option, date );
+        
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
+        
+        // Set value to the select_date element rails
+        if (this.id == 'date_from'){
+          $('#from_day').val(day);
+          $('#from_month').val(month);
+          $('#from_year').val(year);
+        }else {
+          $('#to_day').val(day);
+          $('#to_month').val(month);
+          $('#to_year').val(year);
+        }
+        
+      }
+    },$.datepicker.regional["es"]);
+
 });
