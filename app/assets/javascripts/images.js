225660a727ce41e1eddd3927500c17dd7249ff6e
Raúl Barroso <ra.barroso@gmail.com>
2011-11-01 16:16:32 +0100

    Gallery page finished and optimizied

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,22 +1,32 @@
 jQuery(document).ready(function() {
 
-  Galleria.loadTheme('assets/galleria-themes/galleria.classic.min.js');
-  
-  $('.content').each(function(index) {
-    var set_id = $(this).attr('id');
-    $(this).galleria({
-      flickr: 'set:'+set_id,
-        flickrOptions: {
-          sort: 'date-posted-asc'
-        }
-    });
-  });
+  Galleria.loadTheme('assets/images-theme/galleria.classic.js');
   
   $('.header').click(function(event) {
     event.preventDefault(); 
     event.stopPropagation();
     
-    $(this).parent().children('.gallery').slideToggle('fast');
+    var el = $(this).parent().children('.gallery');
+    
+    el.slideToggle('fast');
+    
+    if (el.find('.galleria-container').length == 0) {
+      
+      var set_id = el.children('.content').attr('id');
+      
+      el.children('.content').galleria({
+        imageCrop:'height',
+        showInfo:false,
+        thumbFit:true,
+        thumbCrop:false,
+        showCounter:false,
+        flickr: 'set:'+set_id,
+          flickrOptions: {
+            sort: 'date-posted-asc'
+          }
+      });
+      
+    }
     
   });
   
