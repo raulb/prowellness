ca1fab99f08888a77b4c3666aa2d5e95f8e7451b
Fernando Blat <ferblape@gmail.com>
2011-11-09 19:32:28 -0300

    Respond to action #show with Ajax

--- app/controllers/book_videos_controller.rb
+++ app/controllers/book_videos_controller.rb
@@ -1,19 +1,23 @@
 # coding: UTF-8
 
 class BookVideosController < ApplicationController
-  
+
   before_filter :visual_guide_login_required
-  
+
   def index
     @books = Book.all
   end
-  
+
   def show
     @books = Book.all
     unless @book_video = BookVideo.where(:code => params[:code]).first
       redirect_to visual_guide_editorial_path, :flash => {:alert => "El código introducido no corresponde a ningún vídeo"} and return
     end
     @book = @book_video.book
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
-  
+
 end
