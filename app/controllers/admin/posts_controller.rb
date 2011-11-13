4c0556eddcddf726a248ffc7328c4698e342d38f
Fernando Blat <ferblape@gmail.com>
2011-11-13 16:08:16 -0300

    Posts filtering in the administration

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,21 +2,43 @@
 
 class Admin::PostsController < ApplicationController
   before_filter :admin_required
-  
+
   layout 'admin'
-  
+
   def index
-    @posts = Post.order("created_at DESC").page(params[:page]).per(10)
+    posts = Post
+    unless params[:q].blank?
+      if params[:q] =~ /^tag:/
+        posts = posts.filter_by_tag(params[:q].split(':').last)
+      else
+        posts = posts.search(params[:q])
+      end
+    end
+    if params[:from] && !params[:from].values.join("").blank?
+      from = Date.parse(params[:from].values.join("/"))
+      posts = posts.where("publish_date >= ?", from)
+    end
+    if params[:to] && !params[:to].values.join("").blank?
+      to = Date.parse(params[:to].values.join("/"))
+      posts = posts.where("publish_date <= ?", to)
+    end
+    unless params[:category].blank?
+      posts = posts.filter_by_category(params[:category])
+    end
+    unless params[:state].blank?
+      posts = posts.where(:state => params[:state])
+    end
+    @posts = posts.order("created_at DESC").page(params[:page]).per(20)
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def edit
     @post = Post.find params[:id]
   end
-  
+
   def update
     @post = Post.find params[:id]
     @post.update_attributes params[:post]
@@ -32,7 +54,7 @@ class Admin::PostsController < ApplicationController
     end
     render "new"
   end
-  
+
   def create
     @post = Post.new params[:post]
     @post.user = current_user
@@ -48,5 +70,5 @@ class Admin::PostsController < ApplicationController
     end
     render "new"
   end
-  
+
 end
