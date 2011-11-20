da0a41431c7299efd9a7bebfa33f95ce9d00ba3c
Fernando Blat <ferblape@gmail.com>
2011-11-20 11:20:37 -0300

    Method get_video_bigger to convert the height and the width of a video to other size

--- config/initializers/string_extension.rb
+++ config/initializers/string_extension.rb
@@ -2,4 +2,12 @@ class String
   def sanitize_sql
     self.gsub(/\\/, '\&\&').gsub(/'/, "''")
   end
+
+  def get_video_bigger
+    unless self.blank?
+      self.gsub("311", "651").gsub("233", "488")
+    else
+      self
+    end
+  end
 end
