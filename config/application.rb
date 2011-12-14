074765d95f4c863f5a1bc4bf76c65577ac8fc68e
Raúl Barroso <ra.barroso@gmail.com>
2011-12-14 21:20:16 +0100

    adding new resources to the application

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,13 @@ module Prowellness
     config.assets.precompile += ['jquery.fileupload-ui.css','images-theme/images-gallery.css']
     
     # CSS
-    config.assets.precompile += ['reset.css', 'fonts.css','about.css', 'admin.css','articles.css','blog_posts.css','books.css','comments.css','images.css','posts.css','search.css','site.css','user_data.css','info.css']
+    config.assets.precompile += ['reset.css', 'fonts.css','about.css', 'admin.css','articles.css','blog_posts.css']
+    config.assets.precompile += ['books.css','comments.css','images.css','posts.css','search.css']
+    config.assets.precompile += ['site.css','user_data.css','info.css','visual-guide.css','visual-guide-editorial.css']
     
     # JS
-    config.assets.precompile += ['about.js','articles.js','books.js','comments.js','images.js','modal_window.js','search.js','user.js']
+    config.assets.precompile += ['about.js','articles.js','books.js','comments.js','images.js','modal_window.js','search.js']
+    config.assets.precompile += ['user.js','visual-guide-editorial.js','visual-guide.js']
     
     # ###### ADMIN
     config.assets.precompile += ['admin/admin.js']
