277c57bafe64073f5e5967896312ca4f0ebc2ded
Raúl Barroso <ra.barroso@gmail.com>
2012-01-12 23:01:28 +0100

    replacing a new size

--- config/initializers/string_extension.rb
+++ config/initializers/string_extension.rb
@@ -6,7 +6,8 @@ class String
   def get_video_bigger
     unless self.blank?
       self.gsub(/width=\"311\"/, 'width="651"').gsub(/height=\"233\"/, 'height="488"').
-           gsub(/height=\"213\"/, 'height="445"').gsub(/height=\"212\"/, 'height="443"')
+           gsub(/height=\"213\"/, 'height="445"').gsub(/height=\"212\"/, 'height="443"').
+           gsub(/height=\"249\"/, 'height="521"')
     else
       self
     end
